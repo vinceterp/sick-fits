936996162c0740e889d5eb60406baffdebee7281
Paul <32608382+vinceterp@users.noreply.github.com>
2020-07-04 20:38:59 -0500

    initial styled components and navbar

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,11 @@
+//Link is bound to React pushState to aid with Routing
+
+const Home = (props) => {
+    return (
+        <div>
+            <h1>Home!</h1>
+        </div>
+    );
+}
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+//Link is bound to React pushState to aid with Routing
+
+const sell = (props) => {
+    return (
+        <div>
+            <h1>Sell!</h1>
+        </div>
+    );
+}
+
+export default sell;
